aa9c21bca81024a35888ead35e273404379a93dd
tedonikolov <teonikolov01@abv.bg>
2024-04-21 11:47:01 +0300

    Added price discount % and subtraction to end price

--- src/componets/ReservationInfo.jsx
+++ src/componets/ReservationInfo.jsx
@@ -19,6 +19,7 @@ import {toast} from "react-toastify";
 import CustomToastContent from "./CustomToastContent";
 import {AuthContext} from "../context/AuthContext";
 import {CurrencyContext} from "../context/CurrencyContext";
+import {HotelContext} from "../context/HotelContext";
 
 export default function ReservationInfo({
                                             reservation,
@@ -26,41 +27,58 @@ export default function ReservationInfo({
                                             setFilter,
                                             setNewReservation,
                                             setShowReservation,
-                                            room
+                                            room,
+                                            fromDate,
+                                            toDate
                                         }) {
     const {t} = useTranslation("translation", {keyPrefix: "common"});
     const {loggedUser} = useContext(AuthContext);
     const {currency, handleRate, currencies} = useContext(CurrencyContext);
+    const {workerHotel} = useContext(HotelContext);
     const [oldCurrency, setOldCurrency] = useState(null);
     const [reservationInfo, setReservationInfo] = useState({
         ...reservation,
-        people: reservation.people ?? 1,
+        peopleCount: reservation.peopleCount ?? 1,
         pricePerNight: 0,
+        priceForNights: 0,
+        priceForMeal: 1,
         price: 0,
+        discount: 0,
+        sub: 0,
+        priceWithDiscount: 0,
         currency: currency
     });
     const [typesOptions, setTypesOptions] = useState([]);
     const [typeId, setTypeId] = useState();
     const [disabledDates, setDisabledDates] = useState();
+    const [mealId, setMealId] = useState();
 
     useEffect(() => {
         setReservationInfo({
             ...reservation,
-            people: reservation.people ?? 1,
+            peopleCount: reservation.peopleCount ?? 1,
             pricePerNight: 0,
+            priceForNights: 0,
+            priceForMeal: 1,
             price: 0,
+            discount: 0,
+            sub: 0,
+            priceWithDiscount: 0,
             currency: currency
         });
-        if(reservation.type){
+        if (reservation.type) {
             setTypeId(reservation.type.id)
+        }
+        if (reservation.meal) {
+            setMealId(reservation.meal.id)
         }
     }, [reservation]);
 
     const {data: types} = useQuery(
         {
-            queryKey: ["get types", filter.hotelId, reservationInfo.people, filter.roomId],
-            queryFn: () => filter.roomId != null ? getTypesForRoom(filter.roomId) : getTypesForRoomByPeopleCount(filter.hotelId, reservationInfo.people),
-            enabled: (reservationInfo.people != null && reservationInfo.people != "" && filter.hotelId!=null) || filter.roomId != null,
+            queryKey: ["get types", filter.hotelId, reservationInfo.peopleCount, filter.roomId],
+            queryFn: () => filter.roomId != null ? getTypesForRoom(filter.roomId) : getTypesForRoomByPeopleCount(filter.hotelId, reservationInfo.peopleCount),
+            enabled: (reservationInfo.peopleCount != null && reservationInfo.peopleCount != "" && filter.hotelId != null) || filter.roomId != null,
             retry: false,
             staleTime: 5000
         });
@@ -74,13 +92,17 @@ export default function ReservationInfo({
     })
 
     const {data: rooms} = useQuery({
-        queryKey: ["get all free rooms for hotel", filter.hotelId && {date: filter.date, hotelId: filter.hotelId, typeId: typeId}],
+        queryKey: ["get all free rooms for hotel", filter.hotelId && {
+            date: filter.date,
+            hotelId: filter.hotelId,
+            typeId: typeId
+        }],
         queryFn: () => getFreeRoomsForDate({...filter, typeId: typeId}),
         enabled: filter.hotelId != null && filter.date !== "Invalid Date" && typeId != null,
     })
 
     const {mutate} = useMutation({
-        mutationFn: () => updateReservation(loggedUser.id, reservationInfo),
+        mutationFn: () => updateReservation(loggedUser.id, {...reservationInfo, price: reservationInfo.priceWithDiscount? reservationInfo.priceWithDiscount : reservationInfo.price}),
         onSuccess: () => {
             queryClient.resetQueries({queryKey: ["get reservation", filter]});
             queryClient.resetQueries({queryKey: ["get unpaid payments", reservation && reservation.customers]});
@@ -89,6 +111,13 @@ export default function ReservationInfo({
                     date: filter.date,
                     hotelId: filter.hotelId,
                     status: filter.status
+                }]
+            });
+            fromDate && queryClient.resetQueries({
+                queryKey: ["get all free room count for hotel", {
+                    fromDate: fromDate,
+                    toDate: toDate,
+                    hotelId: filter.hotelId
                 }]
             });
             toast.success(<CustomToastContent content={[t("updateReservation")]}/>);
@@ -101,6 +130,7 @@ export default function ReservationInfo({
             return {
                 ...prev,
                 price: parseFloat((oldCurrency.price / rate).toFixed(2)),
+                priceForMeal: parseFloat((oldCurrency.priceForMeal / rate).toFixed(2)),
                 pricePerNight: parseFloat((oldCurrency.pricePerNight / rate).toFixed(2)),
             }
         })
@@ -139,14 +169,15 @@ export default function ReservationInfo({
                     ...prev,
                     typeId: type.id,
                     pricePerNight: type.price,
-                    price: type.price * reservationInfo.nights,
+                    priceForNights: (type.price * reservationInfo.nights),
                     currency: type.currency
                 }))
-                setOldCurrency(() => {
+                setOldCurrency((prev) => {
                     return {
+                        ...prev,
                         currency: type.currency,
                         pricePerNight: type.price,
-                        price: type.price * reservationInfo.nights
+                        priceForNights: (type.price * reservationInfo.nights)
                     }
                 });
             }
@@ -155,28 +186,81 @@ export default function ReservationInfo({
                 ...prev,
                 typeId: null,
                 pricePerNight: 0,
-                price: 0,
+                priceForNights: 0,
                 currency: currency
             }))
-            setOldCurrency(() => {
-                return {currency: currency, pricePerNight: 0, price: 0}
+            setOldCurrency((prev) => {
+                return {
+                    ...prev,
+                    currency: currency,
+                    pricePerNight: 0,
+                    priceForNights: 0}
             });
         }
-    }, [typeId, types]);
+    }, [typeId,types]);
 
     useEffect(() => {
-        setReservationInfo((prev) => ({
-            ...prev,
-            price: reservationInfo.pricePerNight * reservationInfo.nights,
-        }))
-        setOldCurrency(() => {
-            return {
-                currency: reservationInfo.currency,
-                pricePerNight: reservationInfo.pricePerNight,
-                price: reservationInfo.pricePerNight * reservationInfo.nights
-            }
-        });
-    }, [reservationInfo.nights]);
+        if (mealId) {
+            let meal = workerHotel.meals.find((meal) => meal.id === mealId);
+            setReservationInfo((prev) => ({
+                ...prev,
+                mealId: meal.id,
+                priceForMeal: meal.price,
+                currency: meal.currency
+            }))
+            setOldCurrency((prev) => {
+                return {
+                    ...prev,
+                    currency: meal.currency,
+                    priceForMeal: meal.price,
+                }
+            });
+        } else {
+            setReservationInfo((prev) => ({
+                ...prev,
+                meal: null,
+                priceForMeal: 0,
+                currency: currency
+            }))
+            setOldCurrency((prev) => {
+                return {
+                    ...prev,
+                    currency: currency,
+                    priceForMeal: 0
+                }
+            });
+        }
+    }, [mealId]);
+
+    useEffect(() => {
+        if(oldCurrency && reservationInfo.currency === oldCurrency.currency) {
+            setReservationInfo((prev) => ({
+                ...prev,
+                price: (reservationInfo.priceForMeal * reservationInfo.peopleCount) + reservationInfo.priceForNights
+            }))
+            setOldCurrency((prev) => {
+                return {
+                    ...prev,
+                    price: (reservationInfo.priceForMeal * reservationInfo.peopleCount) + reservationInfo.priceForNights
+                }
+            });
+        }
+    }, [reservationInfo.priceForNights, reservationInfo.peopleCount, reservationInfo.priceForMeal]);
+
+    useEffect(() => {
+        if(reservationInfo.pricePerNight != 0) {
+            setReservationInfo((prev) => ({
+                ...prev,
+                priceForNights: (reservationInfo.pricePerNight * reservationInfo.nights)
+            }))
+            setOldCurrency((prev) => {
+                return {
+                    ...prev,
+                    priceForNights: (reservationInfo.pricePerNight * reservationInfo.nights)
+                }
+            });
+        }
+    }, [reservationInfo.nights, reservationInfo.pricePerNight]);
 
     useEffect(() => {
         if (!reservationInfo.checkIn) {
@@ -198,13 +282,42 @@ export default function ReservationInfo({
         setNewReservation && setNewReservation((prevValue) => ({...prevValue, ...reservationInfo}));
     }, [reservationInfo]);
 
+    useEffect(() => {
+        setReservationInfo((prev) => ({
+            ...prev,
+            priceWithDiscount: parseFloat(reservationInfo.price * (1 - reservationInfo.discount / 100).toFixed(2))
+        }))
+        setOldCurrency((prev) => {
+            return {
+                ...prev,
+                priceWithDiscount: parseFloat(reservationInfo.price * (1 - reservationInfo.discount / 100).toFixed(2))
+            }
+        });
+
+        setReservationInfo((prev) => ({
+            ...prev,
+            priceWithDiscount: parseFloat(reservationInfo.discount ? (reservationInfo.price * (1 - reservationInfo.discount / 100)) - reservationInfo.sub : reservationInfo.price - reservationInfo.sub).toFixed(2)
+        }))
+        setOldCurrency((prev) => {
+            return {
+                ...prev,
+                priceWithDiscount: parseFloat(reservationInfo.discount ? (reservationInfo.price * (1 - reservationInfo.discount / 100)) - reservationInfo.sub : reservationInfo.price - reservationInfo.sub).toFixed(2)
+            }
+        });
+
+    }, [reservationInfo.sub, reservationInfo.discount, reservationInfo.price]);
+
     function saveReservation(event) {
         mutate();
         event.preventDefault();
     }
 
-    function handleSelect(newValue) {
+    function handleSelectType(newValue) {
         newValue ? setTypeId(newValue.value) : setTypeId(null);
+    }
+
+    function handleSelectMeal(newValue) {
+        newValue ? setMealId(newValue.value) : setMealId(null);
     }
 
     function handleSelectRoom(newValue) {
@@ -218,32 +331,42 @@ export default function ReservationInfo({
         }))
     }
 
-    function handlePriceChange(event) {
-        setReservationInfo((prevState) => ({
-            ...prevState,
-            price: event.target.value,
-            pricePerNight: reservationInfo.nights !== 0 ? parseFloat((event.target.value / reservationInfo.nights).toFixed(2)) : 0,
-        }))
-        setOldCurrency(() => {
-            return {
-                currency: reservationInfo.currency,
-                pricePerNight: parseFloat((event.target.value / reservationInfo.nights).toFixed(2)),
-                price: event.target.value
-            }
-        });
-    }
-
     function handlePricePerNightChange(event) {
         setReservationInfo((prevState) => ({
             ...prevState,
             pricePerNight: event.target.value,
-            price: reservationInfo.nights !== 0 ? event.target.value * reservationInfo.nights : 0,
+            priceForNights: event.target.value * reservationInfo.nights,
+            priceForMeal: reservationInfo.priceForMeal,
+            price: (reservationInfo.priceForMeal * reservationInfo.peopleCount) + (event.target.value * reservationInfo.nights)
         }))
-        setOldCurrency(() => {
+        setOldCurrency((prev) => {
             return {
+                ...prev,
                 currency: reservationInfo.currency,
                 pricePerNight: event.target.value,
-                price: event.target.value * reservationInfo.nights
+                priceForNights: event.target.value * reservationInfo.nights,
+                priceForMeal: reservationInfo.priceForMeal,
+                price: (reservationInfo.priceForMeal * reservationInfo.peopleCount) + (event.target.value * reservationInfo.nights)
+            }
+        });
+    }
+
+    function handlePriceForMealChange(event) {
+        setReservationInfo((prevState) => ({
+            ...prevState,
+            pricePerNight: reservationInfo.pricePerNight,
+            priceForNights: reservationInfo.pricePerNight * reservationInfo.nights,
+            priceForMeal: event.target.value,
+            price: (event.target.value * reservationInfo.peopleCount) + reservationInfo.priceForNights
+        }))
+        setOldCurrency((prev) => {
+            return {
+                ...prev,
+                currency: reservationInfo.currency,
+                pricePerNight: reservationInfo.pricePerNight,
+                priceForNights: reservationInfo.pricePerNight * reservationInfo.nights,
+                priceForMeal: event.target.value,
+                price: (event.target.value * reservationInfo.peopleCount) + reservationInfo.priceForNights
             }
         });
     }
@@ -280,11 +403,11 @@ export default function ReservationInfo({
                     </div>
                     <CommonInputText type={'text'} value={reservationInfo.nights}
                                      label={t('nights')} disabled={true}/>
-                    {filter.hotelId && <CommonInputText name={"people"} label={t("people")}
-                                     setObjectValue={setReservationInfo}
-                                     type={"number"} value={reservationInfo.people}/>}
+                    {filter.hotelId && <CommonInputText name={"peopleCount"} label={t("people")}
+                                                        setObjectValue={setReservationInfo}
+                                                        type={"number"} value={reservationInfo.peopleCount}/>}
                     {filter.hotelId && <CustomSelect
-                        options={typesOptions} defaultValue={typeId} handleSelect={handleSelect}
+                        options={typesOptions} defaultValue={typeId} handleSelect={handleSelectType}
                         label={t("Type")} required={false}
                         isClearable={true}/>}
                     {rooms && typeId && <CustomSelect
@@ -293,11 +416,25 @@ export default function ReservationInfo({
                         })}
                         defaultValue={filter.roomId} handleSelect={handleSelectRoom}
                         label={t("roomName")}/>}
+                    {workerHotel.meals && <CustomSelect
+                        options={workerHotel.meals.map((meal) => ({label: t(meal.type), value: meal.id}))}
+                        defaultValue={mealId} handleSelect={handleSelectMeal}
+                        label={t("Meal")} required={false}
+                        isClearable={true}/>}
+                    <CommonInputText name={"priceForMeal"} label={t("priceForMeal")}
+                                     handleInput={handlePriceForMealChange}
+                                     type={"number"} value={reservationInfo.priceForMeal}/>
                     <CommonInputText name={"pricePerNight"} label={t("pricePerNight")}
                                      handleInput={handlePricePerNightChange}
                                      type={"number"} value={reservationInfo.pricePerNight}/>
-                    <CommonInputText name={"price"} label={t("price")} handleInput={handlePriceChange}
-                                     type={"number"} value={reservationInfo.price}/>
+                    <CommonInputText name={"discount"} label={t("discount")}
+                                     setObjectValue={setReservationInfo}
+                                     type={"number"} value={reservationInfo.discount}/>
+                    <CommonInputText name={"sub"} label={t("sub")}
+                                     setObjectValue={setReservationInfo}
+                                     type={"number"} value={reservationInfo.sub}/>
+                    <CommonInputText name={"price"} label={t("price")} disabled={true}
+                                     type={"number"} value={reservationInfo.priceWithDiscount!=0 ? reservationInfo.priceWithDiscount : reservationInfo.price}/>
                     <div className={"w-45"}><CustomSelect
                         options={currencyOptions.map((currency) => ({
                             label: t(currency.label),
@@ -306,7 +443,7 @@ export default function ReservationInfo({
                         }))}
                         label={t("Currency")} name={"currency"}
                         setObjectValue={setReservationInfo}
-                        defaultValue={reservationInfo.currency} isClearable={true}/></div>
+                        defaultValue={reservationInfo.currency}/></div>
                     {(!setNewReservation && reservation.status !== "FINISHED") &&
                         <Button type={'submit'} className={'register-button'}
                                 disabled={reservation.status === "FINISHED" || reservation.status === "NO_SHOW" || reservation.status === "CANCELLED"}>
